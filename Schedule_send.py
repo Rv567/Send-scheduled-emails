182279d200c79abc6e3b89c74e1f5da910b353ae
Rv567 <chentouf-fcb@hotmail.fr>
2024-07-09 12:10:56 +0100

    req

--- Schedule_send.py
+++ Schedule_send.py
@@ -2,9 +2,8 @@ import streamlit as st
 import schedule
 import time
 from datetime import datetime
-#import threading
-from send_function import *
-
+import threading
+from send_function import send_email
 
 # Streamlit app code
 st.title("My Streamlit App with Scheduler")
@@ -13,16 +12,13 @@ st.write("The function will send emails every 1 minute")
 # Your function to be scheduled
 def my_scheduled_function():
     send_email(sender_email="rachid.chentouf71@gmail.com",
-                                        receiver_email="rachid.axiom@gmail.com",
-                                        subject='Test Email',
-                                        body='This is a test email sent from Python!',
-                                        smtp_server='smtp.gmail.com',
-                                        smtp_port=587,
-                                        app_password='foicfxiwfxwirtjb')
+               receiver_email="rachid.axiom@gmail.com",
+               subject='Test Email',
+               body='This is a test email sent from Python!',
+               smtp_server='smtp.gmail.com',
+               smtp_port=587,
+               app_password='foicfxiwfxwirtjb')
     st.write(f"Email sent at : {datetime.now()}")
-   
-
-schedule.every(1).minute.do(my_scheduled_function)
 
 # Function to run the scheduler
 def run_scheduler():
@@ -30,6 +26,13 @@ def run_scheduler():
         schedule.run_pending()
         time.sleep(1)
 
-run_scheduler()
+# Schedule the function
+schedule.every(1).minutes.do(my_scheduled_function)
 
+# Run the scheduler in a separate thread
+scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
+scheduler_thread.start()
 
+# Streamlit loop to keep the app running
+while True:
+    time.sleep(1)
